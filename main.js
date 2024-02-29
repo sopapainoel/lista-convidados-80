f9fc8dc27747ed75b2967b23c7f0735a2d353a13
sopapainoel <138823838+sopapainoel@users.noreply.github.com>
2024-02-29 18:57:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+ var nameOfTheStudentsArray = [];
+
+ var displayStudentArray = [];
+ 
+
+ for (i =0; i = 0; i < nameOfTheStudentsArray.length; i++) {
+   var nameOfStudent = document.getElementById("nameOfStudent" + i).value;
+   nameOfTheStudentsArray.push(nameOfStudent);
+    
+ }
